85bfae8163ba034e9afec15bb30f29cd008a7293
Juan Camilo Rojas <juanc.1909@gmail.com>
2019-04-04 17:06:35 -0500

    Add public route to images

--- src/components/pages/ProductDetail.js
+++ src/components/pages/ProductDetail.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { API_SERVER } from '../config';
+import { API_SERVER } from '../../config';
 
 // Actions
 import { getProductById } from '../../actions/ProductActions';
